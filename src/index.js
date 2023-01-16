0c1061ba333c1758c8f658aafaf93b4faf89a513
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-01-16 10:33:20 +0200

    Providing the Store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'redux-redux';
 
 import './index.css';
 import App from './App';
+import store from './store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 
 // npm install redux react-redux
 
 // 233 Creating a Redux Store for React
+
+// 234 Providing the Store
